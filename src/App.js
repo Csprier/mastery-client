7d2c5ba95cefdce96183dde40472fb22063fb4a3
Csprier <csprier@gmail.com>
2021-05-25 19:46:05 -0400

    Changed background color to test live reload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-// import DbTestButton from './dbTestButton';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>Mastery Look-up</h1>
-			{/* <DbTestButton /> */}
 		</div>
 	);
 }
