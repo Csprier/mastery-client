7bb70eff4489e54add45ac3dc8a49793abaefbd0
Csprier <csprier@gmail.com>
2021-05-25 22:19:51 -0400

    Fetch works successfully.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+	const [message, setMessage] = useState('');
+
+	async function fetchGreeting() {
+		const res = await fetch('http://localhost:8080/greeting').then((res) =>
+			res.json()
+		);
+		console.log('RES:::', res);
+		setMessage(res.message);
+	}
+
+	useEffect(() => {
+		fetchGreeting();
+	}, [message]);
+
 	return (
 		<div className='App'>
 			<h1>Mastery Look-up</h1>
+			{message !== '' ? message : 'There is a problem...'}
 		</div>
 	);
 }
