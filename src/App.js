ca5101d625f8497bbee7131e9629c3f2f3c2d06c
Csprier <csprier@gmail.com>
2021-06-07 17:20:41 -0400

    Centered the message p element

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Mastery Look-up</h1>
-			{message !== '' ? message : 'There is a problem...'}
+			<p className="message">{message !== '' ? message : 'There is a problem...'}</p>
 		</div>
 	);
 }
