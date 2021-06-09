df4c712d0b4d931af965ca6c2d9f6aa871a5e678
Csprier <csprier@gmail.com>
2021-06-09 12:12:09 -0400

    Created basicitems component and imported into Gathering.jsx, also fixed respective css files

--- src/components/gathering/Gathering.jsx
+++ src/components/gathering/Gathering.jsx
@@ -1,66 +1,13 @@
-import { useEffect, useState } from 'react';
+// components
+import BasicItems from './BasicItems';
 
+// css
 import './gathering.css';
 
 function GatheringData() {
-  const [basicItems, setBasicItemData] = useState([]);
-
-  async function fetchBasicItemData() {
-    const res = await fetch('http://localhost:8080/gathering/basic-items')
-      .then(res => res.json())
-      .catch(e => console.error(e));
-    let basicItemData = res.map(bracket => {
-      return { 
-        mastery: bracket.Mastery,
-        itemChance: bracket.ItemChance,
-        dropAmount: bracket.DropAmount
-      }
-    });
-    console.log('Basic Item Data:::', basicItemData);
-    setBasicItemData(basicItemData);
-  };
-
-  useEffect(() => {
-    if (basicItems.length === 0) {
-      fetchBasicItemData();
-    }
-  }, [basicItems]);
-
-  let basicItemDataRows = basicItems.map(item => {
-    const { mastery, itemChance, dropAmount } = item;
-    return (
-      <tr key={mastery}>
-        <td>{mastery}</td>
-        <td>{itemChance}%</td>
-        <td>{dropAmount}%</td>
-      </tr>
-    );
-  });
-
   return(
-    <div className="basic-items-data">
-
-      <h2>Basic Items</h2>
-
-      <div className="table-display">
-        <table className="fixed-rows">
-          <tbody>
-            <tr>
-              <th>Mastery</th>
-              <th>Item Drop Chance</th>
-              <th>Item Drop Amount</th>
-            </tr>
-          </tbody>
-        </table>
-        <div className="table-container">
-          <table>
-            <tbody>
-              {basicItemDataRows}
-            </tbody>
-          </table>
-        </div>
-      </div>
-
+    <div>
+      <BasicItems />
     </div>
   );
 };
