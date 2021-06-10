beda23899229d84292393cdf229bc858303f8ec5
Csprier <csprier@gmail.com>
2021-06-10 13:49:49 -0400

    Data is being properly rendered and filtered, just needs better css to line things up

--- src/components/gathering/OmniTable.jsx
+++ src/components/gathering/OmniTable.jsx
@@ -1,26 +1,95 @@
 import { useEffect, useState } from 'react';
-import gatheringUTIL from '../../utility/gathering/gathering.util';
+
+// components
+import Filter from './Filter';
 
 function GatheringData() {
   const [data, setData] = useState([]);
+  const [masteries, setMasteries] = useState([]);
+  const [filter, setFilter] = useState('');
+
+
   async function fetchAllGatheringData() {
     const data = await fetch('http://localhost:8080/gathering')
       .then(res => res.json())
-      .then(json => setData(json))
       .catch(e => console.error(e));
-    return data;
-  }
-
-  // const gatheringData = fetchAllGatheringData();
-  // console.log('Gathering data:::', gatheringData);
-
+    let masteriesData = data.map(bracket => { 
+      return { mastery: bracket.mastery }
+    });
+    setData(data);
+    setMasteries(masteriesData);
+  };
+    
   useEffect(() => {
-    // fetchAllGatheringData();
-  }, [data]);
+    if (data.length === 0) {
+      fetchAllGatheringData();
+    }
+  }, [data, masteries]);
+
+  let dataRows = data.map(bracket => {
+    const { 
+      mastery,
+      basic_item_drop_chance,
+      basic_item_drop_amount,
+      rare_resource_drop_chance,
+      rare_resource_drop_amount,
+      special_resource_drop_chance,
+      special_resource_drop_amount,
+      very_rare_resource_drop_chance,
+      very_rare_resource_drop_amount
+    } = bracket;
+    return (
+      <tr key={mastery}>
+        <td>{mastery}</td>
+        <td>{basic_item_drop_chance}%</td>
+        <td>{basic_item_drop_amount}%</td>
+        <td>{rare_resource_drop_chance}%</td>
+        <td>{rare_resource_drop_amount}%</td>
+        <td>{special_resource_drop_chance}%</td>
+        <td>{special_resource_drop_amount}%</td>
+        <td>{very_rare_resource_drop_chance}%</td>
+        <td>{very_rare_resource_drop_amount}%</td>
+      </tr>
+    );
+  });
+
+  let filteredData = data.filter(item => (item.mastery === filter));
+  let filteredDataRows = filteredData.map(bracket => {
+    const { 
+      mastery,
+      basic_item_drop_chance,
+      basic_item_drop_amount,
+      rare_resource_drop_chance,
+      rare_resource_drop_amount,
+      special_resource_drop_chance,
+      special_resource_drop_amount,
+      very_rare_resource_drop_chance,
+      very_rare_resource_drop_amount
+    } = bracket;
+    return (
+      <tr key={mastery}>
+        <td>{mastery}</td>
+        <td>{basic_item_drop_chance}%</td>
+        <td>{basic_item_drop_amount}%</td>
+        <td>{rare_resource_drop_chance}%</td>
+        <td>{rare_resource_drop_amount}%</td>
+        <td>{special_resource_drop_chance}%</td>
+        <td>{special_resource_drop_amount}%</td>
+        <td>{very_rare_resource_drop_chance}%</td>
+        <td>{very_rare_resource_drop_amount}%</td>
+      </tr>
+    );
+  });
+
+  let rowsToRender = (filter) ? filteredDataRows : dataRows;
 
   return(
     <div>
       <h1>Gathering Data!</h1>
+      <Filter 
+        masteries={masteries}
+        setFilter={setFilter}
+      />
       <table className="fixed-rows">
         <tbody>
           <tr>
@@ -39,7 +108,7 @@ function GatheringData() {
       <div className="table-container">
         <table>
           <tbody>
-            
+            {rowsToRender}
           </tbody>
         </table>
       </div>
