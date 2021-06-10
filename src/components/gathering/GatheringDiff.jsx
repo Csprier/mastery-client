a104d5df2dd1ebe4f43d3214c0be83d1857dbbff
Csprier <csprier@gmail.com>
2021-06-10 15:22:28 -0400

    Built range table and rendered it

--- src/components/gathering/GatheringDiff.jsx
+++ src/components/gathering/GatheringDiff.jsx
@@ -3,10 +3,11 @@ import { useEffect, useState } from 'react';
 function GatheringDiff(props) {
   const [m1, setM1] = useState('');
   const [m2, setM2] = useState('');
+  const [range, setRange] = useState([]);
 
   useEffect(() => {
     if (m1.length !== 0 && m2.length !== 0) {
-      doMath(m1, m2)
+      createRangeArray(m1, m2);
     }
   }, [m1, m2]);
 
@@ -19,15 +20,15 @@ function GatheringDiff(props) {
     setM2(range);
   };
 
-  async function doMath(m1, m2) {
+  async function createRangeArray(m1, m2) {
     const data = await fetch('http://localhost:8080/gathering')
       .then(res => res.json())
       .catch(e => console.error(e));
     let range = data.filter((bracket) => {
       return parseInt(bracket.mastery) === parseInt(m1) || parseInt(bracket.mastery) === parseInt(m2);
     });
-    
-  }
+    setRange(range);
+  };
 
   const masteryOptionElementsStart = props.masteries.map(bracket => {
     return (
@@ -53,22 +54,71 @@ function GatheringDiff(props) {
     )
   });
 
+  const rangeRows = range.map(bracket => {
+    const { 
+      mastery,
+      basic_item_drop_chance,
+      basic_item_drop_amount,
+      rare_resource_drop_chance,
+      rare_resource_drop_amount,
+      special_resource_drop_chance,
+      special_resource_drop_amount,
+      very_rare_resource_drop_chance,
+      very_rare_resource_drop_amount
+    } = bracket;
+    return (
+      <tr key={mastery}>
+        <td>{mastery}</td>
+        <td>{basic_item_drop_chance}%</td>
+        <td>{basic_item_drop_amount}%</td>
+        <td>{rare_resource_drop_chance}%</td>
+        <td>{rare_resource_drop_amount}%</td>
+        <td>{special_resource_drop_chance}%</td>
+        <td>{special_resource_drop_amount}%</td>
+        <td>{very_rare_resource_drop_chance}%</td>
+        <td>{very_rare_resource_drop_amount}%</td>
+      </tr>
+    );
+  });
+
   return(
     <div className="gathering-diff">
-      <select
-        id="m1"
-        onChange={setRange1}
-      >
-        <option>Select Mastery A</option>
-        {masteryOptionElementsStart}
-      </select>
-      <select
-        id="m2"
-        onChange={setRange2}
-      >
-        <option>Select Mastery B</option>
-        {masteryOptionElementsStop}
-      </select>
+      <div>
+        <select
+          id="m1"
+          onChange={setRange1}
+        >
+          <option>Select Mastery A</option>
+          {masteryOptionElementsStart}
+        </select>
+        <select
+          id="m2"
+          onChange={setRange2}
+        >
+          <option>Select Mastery B</option>
+          {masteryOptionElementsStop}
+        </select>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>Mastery</th>
+              <th>Basic Item Drop Chance</th>
+              <th>Basic Item Drop Amount</th>
+              <th>Rare Resources Item Drop Chance</th>
+              <th>Rare Resources Item Drop Chance</th>
+              <th>Special Resources Item Drop Chance</th>
+              <th>Special Resources Item Drop Chance</th>
+              <th>Very Rare Resources Item Drop Chance</th>
+              <th>Very Rare Resources Item Drop Chance</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rangeRows}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
