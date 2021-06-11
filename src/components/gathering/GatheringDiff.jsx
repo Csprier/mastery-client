046ae304476ed2d238334d2f445b9e9dce85c825
Csprier <csprier@gmail.com>
2021-06-10 20:45:00 -0400

    Still troubleshooting the infinite loop problem in useeffect

--- src/components/gathering/GatheringDiff.jsx
+++ src/components/gathering/GatheringDiff.jsx
@@ -47,15 +47,40 @@ function GatheringDiff(props) {
    * DATA FUNCTIONS
    * ==========================*/
   useEffect(() => {
-    createRangeState(props);
-  }, [m1, m2, range, props]);
-
-  function createRangeState(props) {
     let range = props.data.filter((bracket) => {
       return parseInt(bracket.mastery) === parseInt(m1) || parseInt(bracket.mastery) === parseInt(m2);
     });
     console.log('range', range);
-  };
+    // setRange(range);
+  }, [m1, m2, range, props]);
+
+
+  const rangeRows = range.map(bracket => {
+    const { 
+      mastery,
+      basic_item_drop_chance,
+      basic_item_drop_amount,
+      rare_resource_drop_chance,
+      rare_resource_drop_amount,
+      special_resource_drop_chance,
+      special_resource_drop_amount,
+      very_rare_resource_drop_chance,
+      very_rare_resource_drop_amount
+    } = bracket;
+    return (
+      <tr key={mastery}>
+        <td>{mastery}</td>
+        <td>{basic_item_drop_chance}%</td>
+        <td>{basic_item_drop_amount}%</td>
+        <td>{rare_resource_drop_chance}%</td>
+        <td>{rare_resource_drop_amount}%</td>
+        <td>{special_resource_drop_chance}%</td>
+        <td>{special_resource_drop_amount}%</td>
+        <td>{very_rare_resource_drop_chance}%</td>
+        <td>{very_rare_resource_drop_amount}%</td>
+      </tr>
+    );
+  });
 
   return (
     <div className="gathering-diff">
@@ -91,8 +116,7 @@ function GatheringDiff(props) {
             </tr>
           </thead>
           <tbody>
-            {/* {rangeRows} */}
-            {/* {calculatedRow} */}
+            {rangeRows}
           </tbody>
         </table>
       </div>
