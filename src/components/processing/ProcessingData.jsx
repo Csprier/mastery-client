ebcc52442dc751923c72171fc224c1c612b98784
Csprier <csprier@gmail.com>
2021-06-17 12:47:12 -0400

    Created processingData.jsx functional component

---
+++ src/components/processing/ProcessingData.jsx
@@ -0,0 +1,41 @@
+import react, { useEffect, useState } = require("react");
+
+// components
+import ProcessingDiff from './ProcessingDiff';
+
+// css
+import './css/processing-diff.css';
+
+function ProcessingData() {
+  const [data, setData] = useState([]);
+  const [masteries, setMasteries] = useState([]);
+
+  async function fetchAllProcessingData() {
+    const data = await fetch('http://localhost:8080/processing')
+      .then(res => res.json())
+      .catch(e => console.error(e));
+    let masteriesData = data.map(bracket => {
+      return { mastery: bracket.mastery }
+    });
+    setData(data);
+    setMasteries(masteriesData);
+  };
+
+  useEffect(() => {
+    if (data.length === 0) {
+      fetchAllProcessingData();
+    }
+  }, [data, masteries]);
+
+  return (
+    <div className="processing-data">
+      <h1>Processing Data</h1>
+      <ProcessingDiff 
+        data={data}
+        masteries={masteries}
+      />
+    </div>
+  );
+};
+
+export default ProcessingData;
---
+++ src/components/processing/ProcessingDiff.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
+// css
+import './css/processing-diff.css';
+
