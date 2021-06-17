1334b9bcf5f64517b0469c6709abfa9a1ad0c5c2
Csprier <csprier@gmail.com>
2021-06-17 12:37:32 -0400

    Created Processing.jsx functional component

---
+++ src/components/processing/Processing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import ProcessingData from './ProcessingData';
+
+// css
+import './css/processing.css';
+
+function Processing() {
+  return (
+    <div className="processing-data-container">
+      <ProcessingData />
+    </div>
+  );
+};
+
+export default Processing;
