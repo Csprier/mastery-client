d5c7e871631d53f6104587be0d8e15116d434839
Csprier <csprier@gmail.com>
2021-06-21 12:38:32 -0400

    Created training.jsx code and css

---
+++ src/components/training/Training.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import TrainingData from './TrainingData';
+
+// css
+import './css/training.css';
+
+function Training() {
+  return (
+    <div className="training-data-container">
+      <TrainingData />
+    </div>
+  );
+};
+
+export default Training;
