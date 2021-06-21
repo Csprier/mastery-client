cdae89a6d44987f2ba723257006742bab3f25de8
Csprier <csprier@gmail.com>
2021-06-21 10:40:58 -0400

    created fishing data functional component and css, also removed unnecessary css from processing-diff.css

---
+++ src/components/fishing/FishingData.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState }from 'react';
+
+// components
+import FishingDiff from './FishingDiff';
+
+// css 
+import './css/fishing-diff.css';
+
+function FishingData() {
+  const [data, setData] = useState([]);
+  const [masteries, setMasteries] = useState([]);
+  
+  async function fetchAllFishingData() {
+    const data = await fetch('http://localhost:8080/fishing')
+      .then(res => res.json())
+      .catch(e => console.error(e));
+    let masteriesData = data.map(bracket => {
+      return { mastery: bracket.mastery }
+    });
+    setData(data);
+    setMasteries(masteriesData);
+  };
+
+  useEffect(() => {
+    if (data.length === 0) {
+      fetchAllFishingData();
+    }
+  }, [data, masteries]);
+
+  return (
+    <div className="fishing-data">
+      <h1>Fishing Data</h1>
+      <FishingDiff 
+        data={data}
+        masteries={masteries}
+      />
+    </div>
+  );
+};
+
+export default FishingData;
