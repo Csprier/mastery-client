446a7176ba90522ae52be14620fff8cf343dedd0
Csprier <csprier@gmail.com>
2021-06-21 10:32:05 -0400

    Created fishing functional component and css

---
+++ src/components/fishing/Fishing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import FishingData from './FishingData';
+
+// css
+import './css/fishing.css';
+
+function Fishing() {
+  return (
+    <div className="fishing-data-container">
+      <FishingData />
+    </div>
+  );
+};
+
+export default Fishing;
