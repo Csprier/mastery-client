37da5e42ca4444a67bfe202715a70fa37af2ed7a
Csprier <csprier@gmail.com>
2021-06-23 15:38:11 -0400

    Created Alchemy and alchemy css code

---
+++ src/components/alchemy/Alchemy.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import AlchemyData from './AlchemyData';
+
+// css
+import './css/alchemy.css';
+
+function Alchemy() {
+  return (
+    <div className="alchemy-data-container">
+      <AlchemyData />
+    </div>
+  );
+};
+
+export default Alchemy;
