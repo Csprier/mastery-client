0d32e557b1b49baefba39b145e4ba5f8ef0aaeaa
Csprier <csprier@gmail.com>
2021-06-23 16:35:20 -0400

    created sailing jsx and css code

---
+++ src/components/sailing/Sailing.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// components
+import SailingData from './SailingData';
+
+// css
+import './css/sailing.css';
+
+function Sailing() {
+  return (
+    <div className="sailing-data-container">
+      <SailingData />
+    </div>
+  );
+};
+
+export default Sailing;
