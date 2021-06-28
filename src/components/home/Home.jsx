305a8ee30cdef0f6652a8e2069ebe39211c78de9
Csprier <csprier@gmail.com>
2021-06-28 13:12:59 -0400

    First version of Home.jsx component is finished. Will work more on images later.

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,12 +3,18 @@ import React from 'react';
 // css
 import './css/home.css';
 import logo from'./images/bdologo.png';
+import pegasus from './images/pegasus.png';
+import witch from './images/witch.png';
 
 function Home(props) {
   console.log(props);
   return (
     <div className="home">
-      <img src={logo} alt="Logo" />
+      <div className="images">
+        <img src={pegasus} className="pegasus" alt="pegasus" />
+        <img src={logo} className="logo" alt="Logo" />
+        <img src={witch} className="witch" alt="witch" />
+      </div>
       <div className="message-container">
         <p className="message">{props.message !== '' ? props.message : 'There is a problem...'}</p>
         <p>Select a life skill, then select 2 masteries and see what you'll gain for increasing your mastery!</p>
