9dea45a658ffd3265febddc4405ca3286fff311a
Csprier <csprier@gmail.com>
2021-06-30 14:58:21 -0400

    Added disclaimer to home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 // css
 import './css/home.css';
 import logo from'./images/bdologo.png';
+import darkknight from './images/darkknight.png';
 import pegasus from './images/pegasus.png';
 import sorceress from './images/sorceress.png';
 import witch from './images/witch.png';
@@ -15,10 +16,12 @@ function Home(props) {
         <img src={sorceress} className="sorceress" alt="sorceress" />
         <img src={logo} className="logo" alt="Logo" />
         <img src={witch} className="witch" alt="witch" />
+        <img src={darkknight} className="darkknight" alt="darkknight" />
       </div>
       <div className="message-container">
         <p className="message">{props.message !== '' ? props.message : 'There is a problem...'}</p>
-        <p>Select a life skill, then select 2 masteries and see what you'll gain for increasing your brackets; or view the entire table!</p>
+        <p className="description">Select a life skill, then select 2 masteries and see what you'll gain for increasing your brackets; or view the entire table!</p>
+        <p className="disclaimer">*(This is not a mobile app)*</p>
       </div>
     </div>
   );
